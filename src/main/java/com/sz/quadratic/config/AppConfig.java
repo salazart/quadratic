bcdedc88bcce8a1c38b57bfc1d3585480d72eb6e
salazart <s.o.w@i.ua>
2016-11-30 21:47:51 +0200

    fix AppConfig

--- src/main/java/com/sz/quadratic/config/AppConfig.java
+++ src/main/java/com/sz/quadratic/config/AppConfig.java
@@ -4,15 +4,14 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.sz.quadratic.dao.impl.QuadraticDaoImpl;
 import com.sz.quadratic.interfaces.IQuadraticDao;
@@ -20,18 +19,9 @@ import com.sz.quadratic.models.Quadratic;
 
 @Configuration
 @EnableTransactionManagement
-@EnableWebMvc
+@ComponentScan({ "com.sz.quadratic.config" })
 public class AppConfig {
 	
-	@Bean
-	public LocalSessionFactoryBean sessionFactory() {
-		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource());
-		sessionFactory.setPackagesToScan(new String[] { "com.sz.quadratic.models" });
-		sessionFactory.setHibernateProperties(hibernateProperties());
-		return sessionFactory;
-	}
-
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -50,14 +40,22 @@ public class AppConfig {
         properties.put("hibernate.hbm2ddl.auto", "create");
         return properties;        
     }
+	@Bean
+	public JpaTransactionManager transactionManager(){
+		JpaTransactionManager transactionManager = new JpaTransactionManager(
+				getEntityManagerFactoryBean().getObject());
+		return transactionManager;
+	}
 	
 	@Bean
-    @Autowired
-    public HibernateTransactionManager transactionManager(SessionFactory s) {
-       HibernateTransactionManager txManager = new HibernateTransactionManager();
-       txManager.setSessionFactory(s);
-       return txManager;
-    }
+	public LocalContainerEntityManagerFactoryBean getEntityManagerFactoryBean(){
+		LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		localContainerEntityManagerFactoryBean.setDataSource(dataSource());
+		localContainerEntityManagerFactoryBean.setPersistenceProvider(new HibernatePersistenceProvider());
+		localContainerEntityManagerFactoryBean.setPackagesToScan("com.sz.quadratic.models");
+		localContainerEntityManagerFactoryBean.setJpaProperties(hibernateProperties());
+		return localContainerEntityManagerFactoryBean;
+	}
 	
 	@Bean
 	public IQuadraticDao quadraticDaoImpl(){
--- src/main/java/com/sz/quadratic/dao/GeneralDao.java
+++ src/main/java/com/sz/quadratic/dao/GeneralDao.java
@@ -1,11 +1,10 @@
 package com.sz.quadratic.dao;
 
-import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,14 +15,17 @@ import com.sz.quadratic.interfaces.IEntity;
 @Transactional
 public class GeneralDao<T extends IEntity> implements IDao<T>{
 
-	@Autowired
-	private SessionFactory sessionFactory;
+//	@Autowired
+//	private SessionFactory sessionFactory;
+	
+	@PersistenceContext
+	private EntityManager em;
 	
 	protected Class<? extends T> clazz;
 	
 	public GeneralDao() {
-		this.clazz = (Class<T>) ((ParameterizedType) getClass()
-				.getGenericSuperclass()).getActualTypeArguments()[0];
+//		this.clazz = (Class<T>) ((ParameterizedType) getClass()
+//				.getGenericSuperclass()).getActualTypeArguments()[0];
 	}
 	
 //	private Session getSession(){
@@ -44,8 +46,9 @@ public class GeneralDao<T extends IEntity> implements IDao<T>{
 	
 
 	public void save(T entity) {
-			 Session session = sessionFactory.getCurrentSession();
-			 session.persist(entity);
+//			 Session session = sessionFactory.getCurrentSession();
+//			 session.persist(entity);
+		em.persist(entity);
 	}
 //
 //	public void update(T entity){
@@ -61,8 +64,8 @@ public class GeneralDao<T extends IEntity> implements IDao<T>{
 	public List<T> getAll() {
 //		return getSession().createQuery("from " + clazz.getName())
 //				.getResultList();
-		Session session = sessionFactory.getCurrentSession();
-		return session.createQuery("from " + clazz.getName())
+//		Session session = sessionFactory.getCurrentSession();
+		return em.createQuery("from " + clazz.getName())
 				.getResultList();
 	}
 
@@ -74,9 +77,9 @@ public class GeneralDao<T extends IEntity> implements IDao<T>{
 
 	@Override
 	public void delete(int id) {
-		Session session = sessionFactory.getCurrentSession();
-		T entity = session.getReference(clazz, id);
-		session.remove(entity);
+//		Session session = sessionFactory.getCurrentSession();
+		T entity = em.getReference(clazz, id);
+		em.remove(entity);
 	}
 
 	@Override
